a83c3b36c1d370296a7748c2d16c77adeec279db
andre <andre1101@naver.com>
2025-06-28 22:39:10 +0900

    first cm

---
+++ src/main/java/com/jeju_nongdi/jeju_nongdi/JejuNongdiApplication.java
@@ -0,0 +1,13 @@
+package com.jeju_nongdi.jeju_nongdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejuNongdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejuNongdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeju_nongdi/jeju_nongdi/JejuNongdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeju_nongdi.jeju_nongdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuNongdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
