52bd3825426b2a824b7da444b207e33b06f7f637
namYeongChan <nyc3884@>
2025-07-01 00:17:22 +0900

    #가상 스레드 적용 -[X]virtual thread 적용
    Close #3

--- src/main/java/com/jeju_nongdi/jeju_nongdi/JejuNongdiApplication.java
+++ src/main/java/com/jeju_nongdi/jeju_nongdi/JejuNongdiApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JejuNongdiApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(JejuNongdiApplication.class, args);
 	}
-
 }
