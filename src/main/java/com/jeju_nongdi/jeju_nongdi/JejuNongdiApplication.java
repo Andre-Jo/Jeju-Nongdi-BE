e02be0bad543422c3cf652b615ce5662909ede32
namYeongChan <nyc3884@>
2025-07-29 16:40:47 +0900

    #createDate 활성화 #35

--- src/main/java/com/jeju_nongdi/jeju_nongdi/JejuNongdiApplication.java
+++ src/main/java/com/jeju_nongdi/jeju_nongdi/JejuNongdiApplication.java
@@ -2,8 +2,10 @@ package com.jeju_nongdi.jeju_nongdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JejuNongdiApplication {
 
 	public static void main(String[] args) {
